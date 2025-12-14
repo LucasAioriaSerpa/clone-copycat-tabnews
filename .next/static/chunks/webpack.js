953a1ab03791beab35f78456de286c906f029ebc
Lucas Aioria Serpa <Lucasaioriaserpa@outlook.com>
2025-12-14 00:43:11 -0300

    reset

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7bc2c4c5997b0ac7"; }
+/******/ 		__webpack_require__.h = function() { return "1e2e8e89bfc4571f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
@@ -1222,11 +1222,6 @@
 /******/ 		var chunkLoadingGlobal = self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || [];
 /******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
 /******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
-/******/ 	}();
-/******/ 	
-/******/ 	/* webpack/runtime/nonce */
-/******/ 	!function() {
-/******/ 		__webpack_require__.nc = undefined;
 /******/ 	}();
 /******/ 	
 /************************************************************************/
--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,7 @@
 
-import styles from '../styles/Home.module.css'
 
 function Home() {
-    return (
-        <main className={styles.container}>
-            <h1 className={styles.title}>hello.. my love~</h1>
-            <h2 className={styles.subtitle}>how's been your day..?</h2>
-            <p className={styles.text}>the silence in my mind screem for you..<br/>
-            i miss you so much it hurts..</p>
-            <p className={styles.text}>i wish you were here with me..<br/>
-            to hold you tight and never let go..
-            </p>
-            <p className={styles.text}>i'm holding my love with all my will <br/>
-            they say what i feel is just because i'm lonely.. but they don't know the truth-
-            </p>
-            <p className={styles.text}>the truth that my heart belongs to you..<br/>
-            and without you, i'm lost in this world..
-            </p>
-            <p className={styles.text}>so please take care my love </p>
-            <p className={styles.text}>and more importantly <br/><br/> don't forget</p>
-            <p className={styles.text}>i'm with you in the dark...</p>
-        </main>
-    )
+    return <h1> hello world !</h1>
 }
 
 export default Home
