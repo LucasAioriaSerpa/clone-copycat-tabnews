7abc9ae3989236a42c131f04a6a5012e1d0b9329
Lucas Aioria Serpa <Lucasaioriaserpa@outlook.com>
2026-01-07 17:55:31 -0300

    configurando "editorconfig" no projeto!

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,15 @@
 import Image from 'next/image'
 
 function Home() {
-    return (
-        <Image
-            loading='eager'
-            src="/images/pablofrases.png"
-            width={1000}
-            height={1000}
-            alt="plablo_frases"
-        />
-    )
+  return (
+    <Image
+      loading='eager'
+      src="/images/pablofrases.png"
+      width={1000}
+      height={1000}
+      alt="plablo_frases"
+    />
+  )
 }
 
 export default Home
