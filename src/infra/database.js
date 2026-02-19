16a4669d9527f266d7f6c61efa9bc9f76d1631dd
Lucas Aioria Serpa <Lucasaioriaserpa@outlook.com>
2026-02-18 21:16:39 -0300

    database.js is now a class and more robust to error handling

--- src/infra/database.js
+++ src/infra/database.js
@@ -1,19 +1,55 @@
 import { Client } from "pg";
 
+class Database {
+  #client;
+  constructor() {
+    this.#client = new Client({
+      host: process.env.POSTGRES_HOST,
+      port: process.env.POSTGRES_PORT,
+      user: process.env.POSTGRES_USER,
+      database: process.env.POSTGRES_DB,
+      password: process.env.POSTGRES_PASSWORD,
+    });
+  }
+
+  getClient() {
+    return this.#client;
+  }
+
+  /** ||>=========================================================================================<||
+   *
+   * @param {string} queryObject
+   * @returns {} Any
+   *
+   * ||>=========================================================================================<||
+   */
+  async query(queryObject) {
+    await this.#client.connect();
+    try {
+      const result = await this.#client.query(queryObject);
+      return result;
+    } catch (error) {
+      console.error(error);
+    } finally {
+      await this.#client.end();
+    }
+  }
+}
+
+/** ||>=========================================================================================<||
+ *
+ * @param {string} queryObject
+ * @returns {} Any
+ *
+ * ||>=========================================================================================<||
+ */
 async function query(queryObject) {
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
-  });
-  await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
+  const database = new Database();
+  const result = await database.query(queryObject);
   return result;
 }
 
 export default {
+  Database: Database,
   query: query,
 };
